66c259efa880e18454940f0a853c63727a73577a
道长 <daozhang.bkg@wayxtech.com>
2021-04-09 14:11:53 +0800

    Init project

---
+++ RGB/RGB.h
@@ -0,0 +1,18 @@
+//
+//  RGB.h
+//  RGB
+//
+//  Created by 白岚花 on 2021/4/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RGB.
+FOUNDATION_EXPORT double RGBVersionNumber;
+
+//! Project version string for RGB.
+FOUNDATION_EXPORT const unsigned char RGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGB/PublicHeader.h>
+
+
